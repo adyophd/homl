8d9eec42f8ee3c68dec39d43bcbead19863821d0
Adam Young <adamyoungphd@gmail.com>
2024-02-29 13:55:54 -0500

    Working code

--- homl_1.1.py
+++ homl_1.1.py
@@ -1,18 +1,13 @@
 # Training and running a linear model in Scikit-Learn
 
-# Install packages in Pycharm > Settings > Project > Interpreter
-# Ideally, automate this
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 # Download and prepare the data
-# Can I grab the data from the original source rather than move it to my own?
-data_root = "https://github.com/ageron/data/raw/main/"
+lifesat = pd.read_csv("https://raw.githubusercontent.com/ageron/data/main/lifesat/lifesat.csv")
 
-lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
 x = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
 
